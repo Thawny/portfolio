01dd40840e0d0bc483c11cd34d8bad6d09327913
Tawny <tony.malto.simon@gmail.com>
2017-09-19 16:18:25 +0200

    transitions landingpage - project ok

--- src/js/hello.js
+++ src/js/hello.js
@@ -1,3 +1,2 @@
 export default function hello() {
-    console.log('helloooooooooo')
 }
--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,30 @@
-import hello from './hello';
 import Rx from 'rxjs/Rx';
 
-var obs = Rx.Observable.of(1,2,3,4,5);
-obs.subscribe(x => console.log("hello"))
+// ===== cache =====
+const body = document.getElementsByTagName('body')
+const container = document.querySelector('.container')
+const overlay = document.getElementById('overlay')
+// end cache
 
-console.log("hello I'm main")
-console.log("hello I'm fuckin maaaaaaaaaaain")
-hello()
+// ===== UI intercations listeners =====
+var dbclick = Rx.Observable.fromEvent(body, 'dblclick')
+                           .map(x => 1)
+                           .scan(count => count + 1, 0)
+dbclick.subscribe(x => x % 2 == 0 ? transitionToLandingPage() : transitionToProject())
+
+// end UI intercations listeners
+
+// ===== UI intercation handles =====
+function transitionToProject() {
+    overlay.classList.remove('hidden')
+    setTimeout(() => overlay.classList.add('to-black'), 100)
+    container.classList.add('to-blur')
+}
+function transitionToLandingPage() {
+    console.log('transition to landing page')
+    overlay.classList.remove('to-black')
+    setTimeout(() => overlay.classList.add('hidden'), 1500)
+    container.classList.remove('to-blur')
+}
+
+// end UI intercations handlers
